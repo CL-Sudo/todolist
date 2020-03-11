e595a1c252e61b8e5fcb0e0b0a4baabea42529ec
CL-Sudo <acl0002@protonmail.ch>
2020-03-11 20:59:56 +0800

    just updated

--- src/modules/todo-list/components/EditForm.js
+++ src/modules/todo-list/components/EditForm.js
@@ -9,76 +9,76 @@ const EditForm = ({ editingValue, setEditingValue }) => {
     });
   };
 
-  const TitleInput = () => {
-    return (
-      <span>
-        <Input
-          type="text"
-          value={editingValue.title}
-          onChange={handleChange}
-          style={{ width: 300 }}
-        />
-      </span>
-    );
-  };
+  // const TitleInput = () => {
+  //   return (
+  //     <span>
+  //       <Input
+  //         type="text"
+  //         value={editingValue.title}
+  //         onChange={handleChange}
+  //         style={{ width: 300 }}
+  //       />
+  //     </span>
+  //   );
+  // };
 
-  const DescriptionInput = () => {
-    return (
-      <span>
-        <Input
-          type="description"
-          value={editingValue.description}
-          onChange={handleChange}
-          style={{ width: 300 }}
-        />
-      </span>
-    );
-  };
+  // const DescriptionInput = () => {
+  //   return (
+  //     <span>
+  //       <Input
+  //         type="description"
+  //         value={editingValue.description}
+  //         onChange={handleChange}
+  //         style={{ width: 300 }}
+  //       />
+  //     </span>
+  //   );
+  // };
+
+  // return (
+  //   <div>
+  //     {JSON.stringify(editingValue, null, 2)};
+  //     <Form layout="vertical">
+  //       <Form.Item label="Title:" name="title">
+  //         <TitleInput />
+  //       </Form.Item>
+  //       <Form.Item label="Description:" name="description">
+  //         <DescriptionInput />
+  //       </Form.Item>
+  //     </Form>
+  //   </div>
+  // );
 
   return (
     <div>
-      {JSON.stringify(editingValue, null, 2)};
-      <Form layout="vertical">
-        <Form.Item label="Title:" name="title">
-          <TitleInput />
-        </Form.Item>
-        <Form.Item label="Description:" name="description">
-          <DescriptionInput />
-        </Form.Item>
-      </Form>
+      <form>
+        <div>
+          <label htmlFor="title">
+            <h3>Title: </h3>
+          </label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            value={editingValue.title}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">
+            <h3>Description: </h3>
+          </label>
+          <input
+            id="description"
+            name="description"
+            type="text"
+            value={editingValue.description}
+            onChange={handleChange}
+          />
+        </div>
+      </form>
     </div>
   );
-
-  //   return (
-  //     <div>
-  //       <form>
-  //         <div>
-  //           <label htmlFor="title">
-  //             <h3>Title: </h3>
-  //           </label>
-  //           <input
-  //             id="title"
-  //             name="title"
-  //             type="text"
-  //             value={editingValue.title}
-  //             onChange={handleChange}
-  //           />
-  //         </div>
-  //         <div>
-  //           <label htmlFor="description">
-  //             <h3>Description: </h3>
-  //           </label>
-  //           <input
-  //             id="description"
-  //             name="description"
-  //             type="text"
-  //             value={editingValue.description}
-  //             onChange={handleChange}
-  //           />
-  //         </div>
-  //       </form>
-  //     </div>
-  //   );
 };
 
 export { EditForm };
